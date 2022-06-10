21b4ed7ab81ab3da68dfa2c90868661fb180f632
«Aleksander <pavel4uk18@gmail.com>
2022-06-10 10:56:21 +0300

    Task1RdyCommit

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+//M = 1; N = 5. -> "1, 2, 3, 4, 5"
+//M = 4; N = 8. -> "4, 6, 7, 8"
+
+
+int GetDemension(string message)
+{
+    Console.WriteLine(message);
+    int demension = int.Parse(Console.ReadLine());
+    return demension;
+}
+
+
+void PrintArrayFromMtoN(int m, int n, int k) // третяя переменная чисто для "расивого вывода"
+{
+    if ( n == m ) Console.Write($"'{m}, ");
+    else 
+    {
+        PrintArrayFromMtoN(m,n-1,k);
+        if (k == n) Console.Write($"{n}'");
+        else Console.Write($"{n}, ");
+    }
+}
+
+int m = GetDemension("Введите число M: ");
+int n = GetDemension("Введите число N: ");
+Console.WriteLine();
+Console.Write($"Выводим натуральные числа в промежутке от M ({m}) до N ({n}) -> ");
+PrintArrayFromMtoN(m,n,n);
